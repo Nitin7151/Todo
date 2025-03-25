32e1931914d9a2fcd880d92d57e27c366ddcc07e
DesignDev App <app@designdev.com>
2025-03-25 11:30:14 +0530

    test13

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
-    import FormComponent from './components/FormComponent.jsx';
+import React, { useState } from 'react';
+    import TodoForm from './components/TodoForm.jsx';
+    import TodoList from './components/TodoList.jsx';
 
     function App() {
+      const [todos, setTodos] = useState([]);
+
+      const addTodo = (task) => {
+        setTodos([...todos, { task, id: Date.now() }]);
+      };
+
+      const removeTodo = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+      };
+
       return (
         <div className="app-container">
-          <h1>Welcome to Our Form</h1>
-          <FormComponent />
+          <h1>Todo App</h1>
+          <TodoForm addTodo={addTodo} />
+          <TodoList todos={todos} removeTodo={removeTodo} />
         </div>
       );
     }
