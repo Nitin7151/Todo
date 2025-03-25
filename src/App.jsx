7deb8470acf41aa284e22bfb355b92821e41dfe3
DesignDev App <app@designdev.com>
2025-03-25 11:08:32 +0530

    changing files to show form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+    import FormComponent from './components/FormComponent.jsx';
+
+    function App() {
+      return (
+        <div className="app-container">
+          <h1>Welcome to Our Form</h1>
+          <FormComponent />
+        </div>
+      );
+    }
+
+    export default App;
